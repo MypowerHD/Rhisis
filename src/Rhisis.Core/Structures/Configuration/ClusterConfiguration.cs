28a864974f19cb7b2bdec4be9da4c0a816ef861e
Timo Pollmeier <timo.p@cryz-games.de>
2019-09-03 01:42:17 +0200

    Fix DataMember Names Missing

--- src/Rhisis.Core/Structures/Configuration/ClusterConfiguration.cs
+++ src/Rhisis.Core/Structures/Configuration/ClusterConfiguration.cs
@@ -12,32 +12,32 @@ namespace Rhisis.Core.Structures.Configuration
         /// <summary>
         /// Gets or sets the cluster server id.
         /// </summary>
-        [DataMember]
+        [DataMember(Name = "id")]
         [DefaultValue(1)]
         public int Id { get; set; }
 
         /// <summary>
         /// Gets or sets the cluster server name.
         /// </summary>
-        [DataMember]
+        [DataMember(Name = "name")]
         public string Name { get; set; }
 
         /// <summary>
         /// Gets or sets the login protect state.
         /// </summary>
-        [DataMember]
+        [DataMember(Name = "enableLoginProtect")]
         public bool EnableLoginProtect { get; set; }
 
         /// <summary>
         /// Gets or sets the Inter-Server configuration.
         /// </summary>
-        [DataMember]
+        [DataMember(Name = "isc")]
         public ISCConfiguration ISC { get; set; } = new ISCConfiguration();
 
         /// <summary>
         /// Gets or sets the default character configuration.
         /// </summary>
-        [DataMember]
+        [DataMember(Name = "defaultCharacter")]
         public DefaultCharacter DefaultCharacter { get; set; } = new DefaultCharacter();
     }
 }
